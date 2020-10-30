eb6e8df01b639f2672db29b491218b310e589d96
NITHYA NARAYANAN C VR <67625225+NithyaNarayananV@users.noreply.github.com>
2020-10-30 21:07:04 +0530

    EXP_1 Record

--- Electricity_Bill.java
+++ Electricity_Bill.java
@@ -1,17 +1,25 @@
 /*
-1. Develop a Java application to generate Electricity bill. Create a class with the following
-members: Consumer no., consumer name, previous month reading, current month reading, type
-of EB connection(i.e domestic or commercial). Compute the bill amount using the following tariff.
+Experiment 1
+PROGRAM TO GENERATE ELECTRICITY BILL
+AIM
+To develop a Java application to generate Electricity bill.
+PROCEDURE
+1.Create a class with the following members Consumer no., consumer name, previous month reading,
+current month reading, type of EB connection (i.e domestic or commercial).
+2.Compute the bill amount using the following tariff.
 If the type of the EB connection is domestic, calculate the amount to be paid as follows:
- First 100 units - Rs. 1 per unit
- 101-200 units - Rs. 2.50 per unit
- 201 -500 units - Rs. 4 per unit
- > 501 units - Rs. 6 per unit
+• First 100 units - Rs. 1 per unit
+• 101-200 units - Rs. 2.50 per unit
+• 201 -500 units - Rs. 4 per unit
+• 501 units - Rs. 6 per unit
 If the type of the EB connection is commercial, calculate the amount to be paid as follows:
- First 100 units - Rs. 2 per unit
- 101-200 units - Rs. 4.50 per unit
- 201 -500 units - Rs. 6 per unit
- > 501 units - Rs. 7 per unit
+• First 100 units - Rs. 2 per unit
+• 101-200 units - Rs. 4.50 per unit
+• 201 -500 units - Rs. 6 per unit
+• 501 units - Rs. 7 per unit
+3. Create the object for the created class .Invoke the methods to get the input from the consumer and
+display the consumer information with the generated electricity bill.
+
 */
 import java.util.Scanner;
 public class Electricity_Bill 
